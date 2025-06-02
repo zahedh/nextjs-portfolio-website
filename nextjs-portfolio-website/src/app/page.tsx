8bd26b0ebb9341d95439ecf9fce025b789a2060c
Zahed Heidari <zahed.heidari2@gmail.com>
2025-06-02 16:42:28 +0100

    Checkpoint
    1) Project is initialised and goes to localhost.
    2) Add a placeholder folder structure.
    3) Started initialising Tailwind CSS. Just need to incorporate the custom file.

---
+++ nextjs-portfolio-website/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-secondary text-black flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Home</h1>
+    </div>
+  );
+}
