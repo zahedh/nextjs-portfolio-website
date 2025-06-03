71e7606c85f026bb2720620045a28f4ad426debb
Zahed Heidari <zahed.heidari2@gmail.com>
2025-06-03 08:43:29 +0100

    Complete
    1) Project launches successfully.
    2) globals.css applies theme correctly and works as a starting point.

--- nextjs-portfolio-website/src/app/page.tsx
+++ nextjs-portfolio-website/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-secondary text-black flex items-center justify-center">
+    <div className="min-h-screen bg-primary text-black flex items-center justify-center">
       <h1 className="text-3xl font-bold">Home</h1>
     </div>
   );
